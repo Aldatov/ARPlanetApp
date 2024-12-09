6643f25f06a0b4581963a17625bd1b2eee937037
Konstantin <aldatovk@gmail.com>
2024-12-09 22:40:33 +0100

    Initial Commit

---
+++ ARPlanetApp/ARPlanetAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ARPlanetAppApp.swift
+//  ARPlanetApp
+//
+//  Created by Konstantin Aldatov on 09/12/24.
+//
+
+import SwiftUI
+
+@main
+struct ARPlanetAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
